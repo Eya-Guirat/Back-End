d0b01fcbedb7178c8ed8c424baab9fcecb0f4979
Eya-Guirat <guirateya8@gmail.com>
2024-03-04 11:48:10 +0100

    first commit

---
+++ AutoReport-2/src/main/java/com/pfe_app/eya/AutoReport2Application.java
@@ -0,0 +1,13 @@
+package com.pfe_app.eya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoReport2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoReport2Application.class, args);
+	}
+
+}
---
+++ AutoReport-2/src/test/java/com/pfe_app/eya/AutoReport2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfe_app.eya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoReport2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
